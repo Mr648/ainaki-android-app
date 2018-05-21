cb03f7eb669b959b2a177dc132e317504025935e
diako_ap <diakoap@gmail.com>
2018-05-21 12:27:01 +0430

    material icon for favorite item

---
+++ app/src/main/java/apps/sffa/com/ainaki/model/Favorite.java
@@ -0,0 +1,20 @@
+package apps.sffa.com.ainaki.model;
+
+/**
+ * Created by Diako on 21/05/2018.
+ */
+
+public class Favorite {
+
+        private int id;
+        private int customerID;
+        private int ProductID;
+        private Customer m_Customer;
+        private Product m_Product;
+
+    public Favorite(){
+
+    }
+    }
+
+
---
+++ app/src/main/java/apps/sffa/com/ainaki/model/Product.java
@@ -0,0 +1,10 @@
+package apps.sffa.com.ainaki.model;
+
+/**
+ * Created by Diako on 21/05/2018.
+ */
+
+public class Product {
+
+
+}
