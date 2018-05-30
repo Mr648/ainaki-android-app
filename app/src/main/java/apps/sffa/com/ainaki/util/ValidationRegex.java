b7de62cac5b0be54033926f90c9d36f9f5141ee8
diako_ap <diakoap@gmail.com>
2018-05-30 11:05:16 +0430

    Before Rahmat Changes

---
+++ app/src/main/java/apps/sffa/com/ainaki/util/ValidationRegex.java
@@ -0,0 +1,13 @@
+package apps.sffa.com.ainaki.util;
+
+/**
+ * Created by Diako on 26/05/2018.
+ */
+
+public class ValidationRegex {
+
+    public  static  final String REGEX_UserEMAIL ="^.*$";
+    public  static  final String REGEX_UserPASS ="^.*$";
+
+
+}
