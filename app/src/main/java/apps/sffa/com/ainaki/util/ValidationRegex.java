aa704a1286c1fbdfca82891f6a5bfba4b2f0c0d7
rahmat waisi <rahmatwaisi@gmail.com>
2018-06-18 11:42:59 +0430

    validation regex modified

--- app/src/main/java/apps/sffa/com/ainaki/util/ValidationRegex.java
+++ app/src/main/java/apps/sffa/com/ainaki/util/ValidationRegex.java
@@ -10,5 +10,10 @@ public class ValidationRegex {
     public  static  final String REGEX_UserPASS ="^.*$";
     public  static  final String REGEX_UserPHONE ="^.*$";
 
+    public static final String REGEX_PHONE
+            = "^(0|\\+98)?([ ]|,|-|[()]){0,2}9[1|2|3|4]([ ]|,|-|[()]){0,2}(?:[0-9]([ ]|,|-|[()]){0,2}){8}$";
+    public static final String REGEX_VERIFICATIONCODE
+            = "^(0-9){4}$";
+
 
 }
